15d0fcf8090136fc32e2afa3a926b0340bc279a5
Maryam Aslam <maryam.aslam@outlook.com>
2020-07-06 02:45:43 +0200

    Extending Avsnitt.js

---
+++ src/Avsnitt.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Avsnitt extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Interpretation</h3>
+                <p>The words of which the initial letter is capitalized have meanings defined under the following conditions.</p>
+                <ul>
+                    <li>element 1</li>
+                    <li>element 2</li>
+                    <li>element 3</li>
+                </ul>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Avsnitt;
--- src/Page.js
+++ src/Page.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Avsnitt from "./Avsnitt";
 
 
 class Page extends Component {
@@ -7,13 +8,9 @@ class Page extends Component {
             <div>
                 <h1>Interpretation and Definitions</h1>
 
-                <h3>Interpretation</h3>
-                <p>The words of which the initial letter is capitalized have meanings defined under the following conditions.</p>
-                <p></p>
+                <Avsnitt />
 
-                <h3>Definitions</h3>
-                <p>For the purposes of this Cookies Policy: Company (referred to as either "the Company", "We", "Us" or "Our" in this Cookies Policy) refers to Northern Lights Aid, Amerikanikou Erithrou Stavrou 104, 65201 Kavala, Greece. </p>
-                <p></p>
+                <Avsnitt />
 
             </div>
         )
